187051ca825e8dd3e4f992909374556594efa0ba
Lucho <lucho.agostino@gmail.com>
2026-06-09 23:58:02 -0300

    Interfaces Dicc y Grafo

--- src/estructurasTDA/IDiccionarioUsuarios.java
+++ src/estructurasTDA/IDiccionarioUsuarios.java
@@ -1,4 +1,10 @@
 package estructurasTDA;
 
+import entidades.Usuario;
+
 public interface IDiccionarioUsuarios {
+    void insertar(int id, Usuario perfil);
+    Usuario buscar(int id);
+    void eliminar(int id);
+    boolean existe(int id);
 }
--- src/estructurasTDA/IGrafoConexiones.java
+++ src/estructurasTDA/IGrafoConexiones.java
@@ -1,4 +1,9 @@
 package estructurasTDA;
 
 public interface IGrafoConexiones {
+    void agregarUsuario(int idUsuario);
+    void eliminarUsuario(int idOrigen, int idDestino);
+    void agregarConexion(int idOrigen, int idDestino);
+    void eliminarConexion(int idOrigen, int idDestino);
+    boolean existeConexion(int idOrigen, int idDestino);
 }
