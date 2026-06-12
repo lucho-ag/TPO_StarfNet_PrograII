602fa936427e8bbef7d6d309b87ca54f1502d592
Juan <juanlvilajosana@gmail.com>
2026-06-12 12:04:22 -0300

    TDA NodoArbolHabilidades

--- src/estructurasTDA/NodoArbolHabilidades.java
+++ src/estructurasTDA/NodoArbolHabilidades.java
@@ -1,4 +1,14 @@
 package estructurasTDA;
+import entidades.Habilidad;
 
 public class NodoArbolHabilidades {
+    public Habilidad habilidad;
+    public NodoArbolHabilidades primerHijo;
+    public NodoArbolHabilidades hermano;
+
+    public NodoArbolHabilidades(Habilidad habilidad) {
+        this.habilidad = habilidad;
+        this.primerHijo = null;
+        this.hermano = null;
+    }
 }
