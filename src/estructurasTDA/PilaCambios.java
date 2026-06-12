37ea32be2b674553a02d537f979a599ad09cca65
Juan <juanlvilajosana@gmail.com>
2026-06-12 12:05:39 -0300

    TDA PilaCambios

--- src/estructurasTDA/PilaCambios.java
+++ src/estructurasTDA/PilaCambios.java
@@ -1,4 +1,26 @@
 package estructurasTDA;
+import entidades.PerfilEstado;
 
-public class PilaCambios {
+public class PilaCambios implements IPilaCambios {
+    private NodoPila tope;
+
+    public PilaCambios() { this.tope = null; }
+
+    @Override
+    public void apilar(PerfilEstado estado) {
+        NodoPila nuevo = new NodoPila(estado);
+        nuevo.sig = tope;
+        tope = nuevo;
+    }
+
+    @Override
+    public PerfilEstado desapilar() {
+        if (estaVacia()) return null;
+        PerfilEstado dato = tope.dato;
+        tope = tope.sig;
+        return dato;
+    }
+
+    @Override
+    public boolean estaVacia() { return tope == null; }
 }
