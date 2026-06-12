6d5566bbb3db5aa45824d9671a17d02cf83ab636
facu14 <facuielpi14@gmail.com>
2026-06-11 23:10:06 -0300

    Se creo clase Usuario que estaba vacia

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -1,4 +1,27 @@
 package entidades;
 
 public class Usuario {
+    private int id;
+    private String nombre;
+
+    public Usuario(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
